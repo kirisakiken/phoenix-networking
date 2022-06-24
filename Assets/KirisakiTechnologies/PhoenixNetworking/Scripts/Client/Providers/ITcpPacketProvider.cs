18855ce5101c7e5dec93ae4d4f83787a46e4a774
kirisakiken <bezmican96@gmail.com>
2022-06-25 00:59:24 +0300

    feat(phoenix-networking): client tcp packet provider implementation

---
+++ Assets/KirisakiTechnologies/PhoenixNetworking/Scripts/Client/Providers/ITcpPacketProvider.cs
@@ -0,0 +1,17 @@
+﻿using KirisakiTechnologies.GameSystem.Scripts.Providers;
+
+namespace KirisakiTechnologies.PhoenixNetworking.Scripts.Client.Providers
+{
+    /// <summary>
+    ///     Packet builder/provider for various network events
+    /// </summary>
+    public interface ITcpPacketProvider : IGameProvider
+    {
+        string DeserializeOnClientConnectedPacket(Packet packet, out int receivedId);
+
+        /// <summary>
+        ///     Packet needs to be build to send welcome received information packet
+        /// </summary>
+        Packet OnConnectWelcomeReceivedPacket(int clientId, string message);
+    }
+}
---
+++ Assets/KirisakiTechnologies/PhoenixNetworking/Scripts/Client/Providers/TcpPacketProvider.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using KirisakiTechnologies.GameSystem.Scripts.Providers;
+
+namespace KirisakiTechnologies.PhoenixNetworking.Scripts.Client.Providers
+{
+    public class TcpPacketProvider : GameProviderBaseMono, ITcpPacketProvider
+    {
+        #region ITcpPacketProvider Implementation
+
+        // TODO: exporting receivedId with out keyword is not good implementation. Find better way e.g. data structures
+        public string DeserializeOnClientConnectedPacket(Packet packet, out int receivedId) // TODO: change return type to data structure
+        {
+            var message = packet.ReadString();
+            var id = packet.ReadInt();
+
+            receivedId = id;
+            return $"Received ID: {id} | Message: {message}";
+        }
+
+        public Packet OnConnectWelcomeReceivedPacket(int clientId, string message) => BuildClientPacket(ClientPackets.ConnectReceived, clientId, message);
+
+        #endregion
+
+        #region Private
+
+        private Packet BuildClientPacket(ClientPackets packetId, int clientId, string message)
+        {
+            var packet = new Packet((int) packetId);
+            packet.Write(clientId);
+            packet.Write(message);
+
+            return packet;
+        }
+
+        #endregion
+    }
+}
