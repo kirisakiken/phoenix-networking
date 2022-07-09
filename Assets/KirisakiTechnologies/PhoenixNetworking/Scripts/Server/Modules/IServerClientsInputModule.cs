380cc29f09c89c4154e631f0c2b8716ddfeff647
kirisakiken <bezmican96@gmail.com>
2022-07-09 16:00:50 +0300

    feat(phoenix-networking/server): add server clients input module

---
+++ Assets/KirisakiTechnologies/PhoenixNetworking/Scripts/Server/Modules/IServerClientsInputModule.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+using KirisakiTechnologies.GameSystem.Scripts.Modules;
+using KirisakiTechnologies.PhoenixNetworking.Scripts.DataTypes;
+
+namespace KirisakiTechnologies.PhoenixNetworking.Scripts.Server.Modules
+{
+    /// <summary>
+    ///     Represents a module that holds information related to
+    ///     network client inputs
+    /// </summary>
+    public interface IServerClientsInputModule : IGameModule
+    {
+        /// <summary>
+        ///     Clients and Inputs collection
+        ///     Map(ClientId, ClientInputs)
+        /// </summary>
+        IReadOnlyDictionary<int, Dictionary<ClientInputKey, bool>> ClientInputs { get; }
+    }
+}
---
+++ Assets/KirisakiTechnologies/PhoenixNetworking/Scripts/Server/Modules/ServerClientsInputModule.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using KirisakiTechnologies.GameSystem.Scripts;
+using KirisakiTechnologies.GameSystem.Scripts.Extensions;
+using KirisakiTechnologies.GameSystem.Scripts.Modules;
+using KirisakiTechnologies.PhoenixNetworking.Scripts.DataTypes;
+
+namespace KirisakiTechnologies.PhoenixNetworking.Scripts.Server.Modules
+{
+    public class ServerClientsInputModule : GameModuleBaseMono, IServerClientsInputModule
+    {
+        #region IServerClientsInputModule
+
+        public IReadOnlyDictionary<int, Dictionary<ClientInputKey, bool>> ClientInputs => _ClientInputs;
+
+        #endregion
+
+        #region Overrides
+
+        public override Task Initialize(IGameSystem gameSystem)
+        {
+            _NetworkEventLogicModule = gameSystem.GetModule<INetworkEventLogicModule>();
+            _NetworkEventLogicModule.OnUdpClientInputPayloadReceived += UdpClientInputPayloadReceivedHandler;
+            // _NetworkEventLogicModule.OnClientDisconnected += ClientDisconnectedHandler; // TODO: remove _ClientInputs[clientId] when client disconnected!
+
+            return base.Initialize(gameSystem);
+        }
+
+        #endregion
+
+        #region Private
+
+        private readonly Dictionary<int, Dictionary<ClientInputKey, bool>> _ClientInputs = new Dictionary<int, Dictionary<ClientInputKey, bool>>();
+
+        private INetworkEventLogicModule _NetworkEventLogicModule;
+
+        private void UdpClientInputPayloadReceivedHandler(int clientId, UdpClientInputPayload payload)
+        {
+            if (!_ClientInputs.ContainsKey(clientId))
+            {
+                var dict = new Dictionary<ClientInputKey, bool>();
+                foreach (ClientInputKey clientInputKey in Enum.GetValues(typeof(ClientInputKey)))
+                    dict.Add(clientInputKey, false);
+
+                _ClientInputs.Add(clientId, dict);
+            }
+
+            foreach (var modifiedInput in payload.ModifiedKeys)
+                _ClientInputs[clientId][modifiedInput.Key] = modifiedInput.Value;
+        }
+
+        #endregion
+    }
+}
